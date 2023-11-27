c007eef71ba8a3fb7762c05ca72e81366153a03f
marzkigun27 <146912399+marzkigun27@users.noreply.github.com>
2023-11-27 11:01:28 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-console. log("ok")
-
 fetch("https://monte-api.vercel.app/api")
-    .then((res) => res.json)
-    .then((data) => console.log(data));
+  .then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("NETWORK RESPONSE ERROR");
+    }
+  })
+  .then(data => {
+    console.log(data);
+    displayUser(data);
+  })
+  .catch((error) => console.error("FETCH ERROR:", error));
+
+  function displayUser(data) {
+    const user = data.namaProvinsi[1];
+    const userDiv = document.getElementById("user-id"); 
+    const userProvinsi = user.namaProvinsi;
+    const heading = document.createElement("h1");
+    heading.innerHTML = userProvinsi;
+    userDiv.appendChild(heading);
+  }   
